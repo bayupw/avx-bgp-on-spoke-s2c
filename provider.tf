e90ad28855da44ab72fa1807f3dd0521a61872a5
bayupw <bayupw@gmail.com>
2022-01-25 17:52:30 +1300

    update avx provider to 6.6

--- provider.tf
+++ provider.tf
@@ -5,7 +5,7 @@ terraform {
     }
     aviatrix = {
       source  = "AviatrixSystems/aviatrix"
-      version = ">= 2.15"
+      version = "2.21.0-6.6.ga" #6.6
     }
     tls = {
       source  = "hashicorp/tls"
